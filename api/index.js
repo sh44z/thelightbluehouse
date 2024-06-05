05698a6ccea922728ebff3bda3aa1f9bb4d6eba6
Shehzad Khan <sh44z@live.co.uk>
2024-06-05 16:26:30 +0100

    changed cross origin 5173 to netlify

--- api/index.js
+++ api/index.js
@@ -25,7 +25,7 @@ app.use(cookieParser())
 app.use("/uploads", express.static(__dirname + "/uploads"))
 app.use(cors({
     credentials: true,
-    origin: 'http://localhost:5173'
+    origin: 'transcendent-zuccutto-c4687b.netlify.app'
 }));
 
 
